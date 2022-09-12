b2c8792912eae3b1bce86cf0db930f896d66c853
parmyspn <parmisprv@gmail.com>
2022-09-12 10:25:29 -0700

    Adjusted the comments

--- Image-Compressor/ptree.cpp
+++ Image-Compressor/ptree.cpp
@@ -1,62 +1,31 @@
-/*
- *  File:        ptree.cpp
- *  Description: Implementation of a partitioning tree class for CPSC 221 PA3
- *  Date:        2022-03-03 01:53
- *
- *               ADD YOUR PRIVATE FUNCTION IMPLEMENTATIONS TO THE BOTTOM OF THIS FILE
- */
+
 
 #include "ptree.h"
-#include "hue_utils.h" // useful functions for calculating hue averages
+#include "hue_utils.h" 
 
 using namespace cs221util;
 using namespace std;
 
-// The following definition may be convenient, but is not necessary to use
+
 typedef pair<unsigned int, unsigned int> pairUI;
 
-/////////////////////////////////
-// PTree private member functions
-/////////////////////////////////
 
-/*
- *  Destroys all dynamically allocated memory associated with the current PTree object.
- *  You may want to add a recursive helper function for this!
- *  POST: all nodes allocated into the heap have been released.
- */
+
 void PTree::Clear()
 {
-  // add your implementation below
   remove_all(root);
 }
 
-/*
- *  Copies the parameter other PTree into the current PTree.
- *  Does not free any memory. Should be called by copy constructor and operator=.
- *  You may want a recursive helper function for this!
- *  PARAM: other - the PTree which will be copied
- *  PRE:   There is no dynamic memory associated with this PTree.
- *  POST:  This PTree is a physically separate copy of the other PTree.
- */
+
 void PTree::Copy(const PTree &other)
 {
   root = add_recursive(root, other.root);
 }
 
-/*
- *  Private helper function for the constructor. Recursively builds the tree
- *  according to the specification of the constructor.
- *  You *may* change this if you like, but we have provided here what we
- *  believe will be sufficient to use as-is.
- *  PARAM:  im - full reference image used for construction
- *  PARAM:  ul - upper-left image coordinate of the currently building Node's image region
- *  PARAM:  w - width of the currently building Node's image region
- *  PARAM:  h - height of the currently building Node's image region
- *  RETURN: pointer to the fully constructed Node
- */
+
 Node *PTree::BuildNode(PNG &im, pair<unsigned int, unsigned int> ul, unsigned int w, unsigned int h)
 {
-  // replace the line below with your implementation
+
 
   if (w == 0 || h == 0)
   {
@@ -100,51 +69,11 @@ Node *PTree::BuildNode(PNG &im, pair<unsigned int, unsigned int> ul, unsigned in
   return result;
 }
 
-////////////////////////////////
-// PTree public member functions
-////////////////////////////////
+
+
 
 /*
  *  Constructor that builds the PTree using the provided PNG.
- *
- *  The PTree represents the sub-image (actually the entire image) from (0,0) to (w-1, h-1) where
- *  w-1 and h-1 are the largest valid image coordinates of the original PNG.
- *  Each node corresponds to a rectangle of pixels in the original PNG, represented by
- *  an (x,y) pair for the upper-left corner of the rectangle, and two unsigned integers for the
- *  number of pixels on the width and height dimensions of the rectangular sub-image region the
- *  node defines.
- *
- *  A node's two children correspond to a partition of the node's rectangular region into two
- *  equal (or approximately equal) size regions which are either tiled horizontally or vertically.
- *
- *  If the rectangular region of a node is taller than it is wide, then its two children will divide
- *  the region into vertically-tiled sub-regions of equal height:
- *  +-------+
- *  |   A   |
- *  |       |
- *  +-------+
- *  |   B   |
- *  |       |
- *  +-------+
- *
- *  If the rectangular region of a node is wider than it is tall, OR if the region is exactly square,
- *  then its two children will divide the region into horizontally-tiled sub-regions of equal width:
- *  +-------+-------+
- *  |   A   |   B   |
- *  |       |       |
- *  +-------+-------+
- *
- *  If any region cannot be divided exactly evenly (e.g. a horizontal division of odd width), then
- *  child B will receive the larger half of the two subregions.
- *
- *  When the tree is fully constructed, each leaf corresponds to a single pixel in the PNG image.
- *
- *  For the average colour, this MUST be computed separately over the node's rectangular region.
- *  Do NOT simply compute this as a weighted average of the children's averages.
- *  The functions defined in hue_utils.h and implemented in hue_utils.cpp will be very useful.
- *  Computing the average over many overlapping rectangular regions sounds like it will be
- *  inefficient, but as an exercise in theory, think about the asymptotic upper bound on the
- *  number of times any given pixel is included in an average calculation.
  *
  *  PARAM: im - reference image which will provide pixel data for the constructed tree's leaves
  *  POST:  The newly constructed tree contains the PNG's pixel data in each leaf node.
@@ -164,7 +93,7 @@ PTree::PTree(PNG &im)
  */
 PTree::PTree(const PTree &other)
 {
-  // add your implementation below
+  
   Copy(other);
 }
 
@@ -209,8 +138,7 @@ PTree::~PTree()
  */
 PNG PTree::Render() const
 {
-  // replace the line below with your implementation
-  // TODO
+
   PNG im;
   im.resize(root->width, root->height);
   render_helper(im, root);
@@ -226,7 +154,6 @@ PNG PTree::Render() const
  *  When processing a subtree, you should determine if the subtree should be pruned,
  *  and prune it if possible before determining if it has subtrees that can be pruned.
  *
- *  You may want to add (a) recursive helper function(s) for this!
  *
  *  PRE:  This tree has not been previously pruned (and is not copied/assigned from a tree that has been pruned)
  *  POST: Any subtrees (as close to the root as possible) whose leaves all have colour
@@ -235,7 +162,7 @@ PNG PTree::Render() const
  */
 void PTree::Prune(double tolerance)
 {
-  // add your implementation below
+ 
   prune_helper(root, tolerance);
 }
 
@@ -266,9 +193,7 @@ int PTree::NumLeaves() const
 /*
  *  Rearranges the nodes in the tree, such that a rendered PNG will be flipped horizontally
  *  (i.e. mirrored over a vertical axis).
- *  This can be achieved by manipulation of the nodes' member attribute(s).
- *  Note that this may possibly be executed on a pruned tree.
- *  This function should run in time linearly proportional to the size of the tree.
+ *  This function runs in time linearly proportional to the size of the tree.
  *
  *  You may want to add a recursive helper function for this!
  *
@@ -276,16 +201,14 @@ int PTree::NumLeaves() const
  */
 void PTree::FlipHorizontal()
 {
-  // add your implementation below
+ 
   horizonntal_helper(root);
 }
 
 /*
  *  Like the function above, rearranges the nodes in the tree, such that a rendered PNG
  *  will be flipped vertically (i.e. mirrored over a horizontal axis).
- *  This can be achieved by manipulation of the nodes' member attribute(s).
- *  Note that this may possibly be executed on a pruned tree.
- *  This function should run in time linearly proportional to the size of the tree.
+ *  This function should runs in time linearly proportional to the size of the tree.
  *
  *  You may want to add a recursive helper function for this!
  *
@@ -293,22 +216,19 @@ void PTree::FlipHorizontal()
  */
 void PTree::FlipVertical()
 {
-  // add your implementation below
   vertical_helper(root);
 }
 
 /*
  *  Provides access to the root of the tree.
- *  Dangerous in practice! This is only used for testing.
+ *  This is only used for testing.
  */
 Node *PTree::GetRoot()
 {
   return root;
 }
 
-//////////////////////////////////////////////
-// PERSONALLY DEFINED PRIVATE MEMBER FUNCTIONS
-//////////////////////////////////////////////
+
 
 void PTree::remove_all(Node *ptr)
 {
